93e621ffc3b7de4f2dfbe8a862e4f2c328940de4
Carlo Lobrano <c.lobrano@gmail.com>
2022-08-20 11:29:49 +0200

    setup: install vagrant upstream

--- host-install-dependencies.sh
+++ host-install-dependencies.sh
@@ -2,8 +2,18 @@
 # -*- coding: UTF-8 -*-
 
 set -x
-sudo apt install -y virt-manager qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
 
+# Install upstream vagrant (https://www.vagrantup.com/downloads)
+if command -v apt>/dev/null; then
+    wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt update
+    sudo apt install -y vagrant virt-manager qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils 
+fi
+
+# Add user to libvirt group
 sudo adduser `id -un` libvirt
 sudo adduser `id -un` kvm
 newgrp libvirt
+
+vagrant plugin install vagrant-libvirt
